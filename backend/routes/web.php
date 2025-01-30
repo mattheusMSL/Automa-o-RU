6d6bd8647514c598c401c1ce09efca2a65ceee11
Matheus Santos Lopes <matthewpeterparker@gmail.com>
2025-01-30 06:24:27 -0300

    add routes test to test the db migration

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,3 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return 'rodando';
+});
+
+Route::get('/users', [UserController::class, 'index']);
+
+route::get('/users/{id}', [UserController::class, 'show']);
