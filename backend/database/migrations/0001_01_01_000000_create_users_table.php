e22e9bb595be8e5d85c5e261f7f2d7cda92fb3dc
Matheus Santos Lopes <matthewpeterparker@gmail.com>
2025-01-30 19:02:53 -0300

    add extra conection to migrations

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration  {
+    
+    protected $connection = 'pgsql';
+
     /**
      * Run the migrations.
      */
